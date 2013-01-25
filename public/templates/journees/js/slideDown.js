7a3e9f43cb3e06a6e81a07afec8f2b3a127df325
nicolas.duvollet@webtales.fr <duvolletn@HP-Folio-13-Nicolas-Duvollet.(none)>
2013-01-25 16:24:29 +0100

    update slideDown function

--- public/templates/journees/js/slideDown.js
+++ public/templates/journees/js/slideDown.js
@@ -3,13 +3,17 @@
     var self=$(this);  
 	if(self.css("display")=="block")
 	{
-		self.slideUp("slow");
-		$("#overflow").hide();
+		self.slideUp("slow",function(){
+			$("#overflow").hide();
+		});
+		
 	}
 	else
 	{
-		self.slideDown("slow");
-		$("#overflow").show();
+		self.slideDown("slow",function(){
+			$("#overflow").show();
+		});
+		
 	}
 	$(".info").each(function(){
 		if($(this).css("display")=="block" && $(this).attr("id")!=self.attr("id"))
